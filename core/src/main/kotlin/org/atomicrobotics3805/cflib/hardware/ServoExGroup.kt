e6ce896775f8bb10ab9ba224682ab91bce2eab8b
REM-Codes <rowan.mcalpin@gmail.com>
2023-05-12 08:35:37 -0700

    Refactor ServoEx & ServoConversion
    Seperate ServoConversion into a new file.
    
    Remove 'open' keyword from ServoExGroup
    
    Co-Authored-By: AdamMEI <53022562+AdamMEI@users.noreply.github.com>

--- core/src/main/kotlin/org/atomicrobotics3805/cflib/hardware/ServoExGroup.kt
+++ core/src/main/kotlin/org/atomicrobotics3805/cflib/hardware/ServoExGroup.kt
@@ -2,7 +2,7 @@ package org.atomicrobotics3805.cflib.hardware
 
 import com.qualcomm.robotcore.hardware.Servo
 
-open class MultiServoEx(
+class ServoExGroup(
     vararg val servos: ServoConversion
 ): ServoEx(servos[0].name, servos[0].type, servos[0].speedOverride) {
 
@@ -49,20 +49,4 @@ open class MultiServoEx(
             it.initialize()
         }
     }
-}
-
-class ServoConversion(
-    name: () -> String, type: ServoType? = null, speedOverride: Double? = null,
-    val positionTranslator: (Double) -> Double,
-    val directionTranslator: (Servo.Direction?) -> Servo.Direction? = { it },
-    val scaleRangeTranslator: (Double, Double) -> Pair<Double, Double> = { min, max -> Pair(min, max)}
-): ServoEx(name, type, speedOverride) {
-
-    constructor(
-        name: String, type: ServoType? = null, speedOverride: Double? = null,
-        positionTranslator: (Double) -> Double,
-        directionTranslator: (Servo.Direction?) -> Servo.Direction? = { it },
-        scaleRangeTranslator: (Double, Double) -> Pair<Double, Double> = { min, max -> Pair(min, max)}):
-            this({ name }, type, speedOverride, positionTranslator,
-                directionTranslator, scaleRangeTranslator)
 }
